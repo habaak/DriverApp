589dbc8f6f0fda48c0a9504268aaccce2fd14d76
habaak <joohn728@gmail.com>
2018-05-28 17:30:04 +0900

    ADD driverApp

--- app/src/main/java/com/erise/habaak/driverapp/HomeFragment.java
+++ app/src/main/java/com/erise/habaak/driverapp/HomeFragment.java
@@ -1,6 +1,12 @@
 package com.erise.habaak.driverapp;
 
+import android.content.Context;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,16 +16,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.lang.Runnable;
@@ -45,12 +71,21 @@ public class HomeFragment extends Fragment {
     private String mParam2;
 
     private Server server;
-    private TextView temp;
-    private TextView humi;
+    private String url = "http://192.168.0.24/first/admin/";
+    private TextView tempView;
+    private TextView humiView;
 
     private TextView mTextMessage;
-    private TimerTask timerTask;
+    String lon, lat;
+
+    private TimerTask timerTempHumiTask;
+    private TimerTask speedTask;
+    private TimerTask locationTask;
     private Timer timer;
+
+    private LatLonSender latLonSender;
+    private LocationManager manager;
+    private GPSListener gpsListener;
 
     private OnFragmentInteractionListener mListener;
 
@@ -83,18 +118,37 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
+        startLocationService();
         try {
             server = new Server();
             server.start();
-            timerTask = new TimerTask() {
+//Timer
+            timerTempHumiTask = new TimerTask() {
                 @Override
                 public void run() {
-                    server.sendAll();
+                    server.sendTempHumi();
                 }
             };
+
+           speedTask = new TimerTask() {
+                @Override
+                public void run() {
+                    server.sendSpeed();
+                }
+            };
+
+            locationTask = new TimerTask() {
+                @Override
+                public void run() {
+                    latLonSender = (LatLonSender) new LatLonSender().execute(url+"relocation.do",lat,lon);
+                }
+            };
+
+
             timer = new Timer();
-            timer.schedule(timerTask,0,3000);
+            timer.schedule(timerTempHumiTask,0,5000);
+            timer.schedule(speedTask,0,3000);
+            timer.schedule(locationTask,0,3000);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -109,8 +163,8 @@ public class HomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         //  TCP IP
-        temp = v.findViewById(R.id.temp);
-        humi = v.findViewById(R.id.humi);
+        tempView = v.findViewById(R.id.temp);
+        humiView = v.findViewById(R.id.humi);
 
         //  RecyclerView
         RecyclerView recyclerView = v.findViewById(R.id.recyclerview);
@@ -196,23 +250,26 @@ public class HomeFragment extends Fragment {
 
 //  TCP IP Server
 /*
-    온도, 습도, 차량정보
+    온도
 */
-    public void setTemp(final String msg){
+    public void setTempView(final String tempNum, final String humiNum){
         Runnable r = new Runnable() {
             @Override
             public void run() {
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        temp.setText(msg);
+                        Log.i("[setTempView]",tempNum + " : "+ humiNum);
+
+                        tempView.setText(tempNum);
+                        humiView.setText(humiNum);
                     }
                 });
             }
         };
         new Thread(r).start();
     }
-    public void setHumi(final String msg){
+    /*public void setHumi(final String msg){
         Runnable r = new Runnable() {
             @Override
             public void run() {
@@ -225,7 +282,7 @@ public class HomeFragment extends Fragment {
             }
         };
         new Thread(r).start();
-    }
+    }*/
 
     public class Server extends Thread{
 
@@ -249,7 +306,7 @@ public class HomeFragment extends Fragment {
                     Socket socket = serverSocket.accept();
                     String client = socket.getInetAddress().getHostAddress();
                     new Receiver(socket).start();
-                    new Sender();
+                    //new Sender();
                 }
             } catch(Exception e) {
                 e.printStackTrace();
@@ -279,7 +336,6 @@ public class HomeFragment extends Fragment {
             }
 
 
-
             @Override
             public void run() {
                 try {
@@ -287,9 +343,21 @@ public class HomeFragment extends Fragment {
                     while(rflag) {
                         if(socket.isConnected() && dis != null & dis.available() > 0) {
                             String str = dis.readUTF();
-                            setTemp(str);
-                            /*SendHttp sendHttp = new SendHttp(str);
-                            sendHttp.execute();*/
+                            Log.i("[Receive MSG]",str);
+                            //조건문으로 통신 값 분류
+
+                            StringTokenizer st = new StringTokenizer(str, " ");
+                            String[] tokenBox = new String[10];
+                            for( int x = 0; st.hasMoreElements(); x++ ){
+                               //Log.i("[Token]" , x + " : " + st.nextToken() );
+                               tokenBox[x]=st.nextToken();
+                                Log.i("[Receive MSG array]",tokenBox[x]);
+                            }
+
+                            Log.i("[Token]",tokenBox[1] +":"+tokenBox[3]);
+                            setTempView(tokenBox[1],tokenBox[3]); //화면 온도 변화
+                            latLonSender = (LatLonSender) new LatLonSender().execute(url+"relocation.do",lat,lon);
+                            SendTempHumiHttp sendTempHumiHttp = (SendTempHumiHttp) new  SendTempHumiHttp().execute(tokenBox[1],tokenBox[3]);
                         }
                     }
 
@@ -331,15 +399,23 @@ public class HomeFragment extends Fragment {
                 }
             }
         }
-        public void sendAll() {
-            Log.i("[Server App]","TEMPELATURE");
-            Sender sender = new Sender();
+        public void sendTempHumi() {
+            Log.i("[TEMPELATURE]","send");
+            TempHumiSender sender = new TempHumiSender();
             //sender.setMsg(msg);
             sender.start();
         }
 
+        public void sendSpeed(){
+            Log.i("[SPEED]","send");
+            SpeedSender sender = new SpeedSender();
+            //sender.setMsg(msg);
+            sender.start();
+        }
         //Send Message All Clients
-        class Sender extends Thread{
+
+//온습도 sender
+        class TempHumiSender extends Thread{
             /*String msg;
             public void setMsg(String msg) {
                 this.msg = msg;
@@ -358,26 +434,224 @@ public class HomeFragment extends Fragment {
                 }
             }
         }
+//속도 sender
+        class SpeedSender extends Thread{
+            /*String msg;
+            public void setMsg(String msg) {
+                this.msg = msg;
+            }*/
+
+            @Override
+            public void run() {
+                try {
+                    if(!list.isEmpty() && list.size()>=0) {
+                        for(DataOutputStream dos : list) {
+                            dos.writeUTF("v");
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
         public void stopServer() {
             rflag = false;
-
         }
 
     }
+//LAT, LON
+    private void startLocationService() {
+
+        manager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        gpsListener = new GPSListener();
+        long minTime = 1000;
+        float minDistance = 0;
+        try {
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, gpsListener);
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                Double latitude = lastLocation.getLatitude();
+                Double longitude = lastLocation.getLongitude();
+                //textView.setText("내 위치 : " + latitude + ", " + longitude);
+            }
+
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+    private class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            lat = String.valueOf(location.getLatitude());
+            lon = String.valueOf(location.getLongitude());
+            String msg = "Latitude : "+ lat + "\nLongitude:"+ lon; Log.i("GPSListener", msg);
+            //textView.setText("내 위치는 : " + latitude + ", " + longitude);
+            Toast.makeText(getActivity().getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+
+            manager.removeUpdates(gpsListener);
+
+        }
+        public void onProviderDisabled(String provider) {
+
+        } public void onProviderEnabled(String provider) {
+
+        } public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    }
+
+
+
+    //==================latlon Networking============================
+    public class LatLonSender extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            getMainJson(strings[0],strings[1],strings[2]);
+            return null;
+        }
+
+        private void getMainJson(String url,String lat, String lon){
+
+            try{
+                HttpClient client = new DefaultHttpClient();
+                HttpPost post = new HttpPost(url);
+                Log.i("[LatLonURL]",url);
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                nameValuePairs.add(new BasicNameValuePair("lat", lat));
+                nameValuePairs.add(new BasicNameValuePair("lon", lon));
+                nameValuePairs.add(new BasicNameValuePair("busidx", "5"));
+                nameValuePairs.add(new BasicNameValuePair(HTTP.CONTENT_TYPE,"application/json"));
+
+                post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                HttpResponse response = client.execute(post);
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    Log.i("[Location JSON]",line);
+
+                    JSONArray jsonArray = new JSONArray(line);
+                    System.out.println("JSONArray -- "+jsonArray);
+
+                    for(int i = 0 ; i < jsonArray.length(); i++){
+                        /*JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        //String isSuccess = jsonObject.getString("MainSucess");
+                        *//**//*cmt = jsonObject.getString("cmt");
+                        uidx = jsonObject.getString("uidx");
+                        pidx = jsonObject.getString("pidx");
+                        date = jsonObject.getString("dt");
+                        like = jsonObject.getString("heart");*//**//*
+                        cmtArr[i] = jsonObject.getString("cmt");
+                        uidxArr[i] = jsonObject.getString("uidx");
+                        pidxArr[i] = jsonObject.getString("pidx");
+                        dateArr[i] = jsonObject.getString("dt");
+                        likeArr[i] = jsonObject.getString("heart");
+                        //imgurlArr[i] = jsonObject.getString("img");*/
+                    }
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }finally {
+            }
+
+        }
+    }
+}
+
+
+
+
 
 //ServerSocket Stop....
 
-//  HTTP Client
-    /*class SendHttp extends AsyncTask<Void,Void,Void>{
+//  HTTP Client Post
+    /*class SendHttp extends AsyncTask<String,Void,Void> {
 
-        String surl = "http://70.12.114.132:8070/webserver/main.do?";
+        //String surl = "http://192.168.0.24/first/admin/retemp.do?temp=27&humid=30&busidx=5";
+        String url = "http://192.168.0.24/first/admin/";
+
+        String controller;
+        String str;
+
+        public SendHttp(String controller, String deviceid){
+            controller = this.controller;
+            deviceid = this.str;
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                String controller = params[0];
+                String str = params[1];
+                URL url = new URL("http://192.168.0.24/first/admin/"+controller+"?");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("content-type","application/x-www-form-urlencoded");
+//Making url
+                StringBuffer buffer = new StringBuffer();
+                buffer.append("temp").append("=").append(str).append("&");
+                buffer.append("humid").append("=").append(str).append("&");
+                buffer.append("busidx").append("=").append("5");
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                PrintWriter wr = new PrintWriter(osw);
+                wr.write(buffer.toString());
+                wr.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }*/
+    class SendTempHumiHttp extends AsyncTask<String,Void,Void> {
+        String surl = "http://192.168.0.24/first/admin/retemp.do?";
+
+        String tempNum;
+        String HumidNum;
+        HttpURLConnection urlConn;
+        URL url;
+        public SendTempHumiHttp(){}
+        public SendTempHumiHttp(String tempNum, String HumidNum){
+            HumidNum = this.HumidNum;
+            tempNum = this.tempNum;
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                String surl = "http://192.168.0.24/first/admin/retemp.do?";
+                StringBuffer buffer = new StringBuffer();
+                buffer.append(surl);
+                buffer.append("temp").append("=").append(params[0]).append("&");
+                buffer.append("humid").append("=").append(params[1]).append("&");
+                buffer.append("busidx").append("=").append("5");
+                surl = buffer.toString();
+                Log.i("[URL-retemp]",surl);
+                try {
+                    url = new URL(surl);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.getResponseCode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+   /* class SendTempHumiHttp extends AsyncTask<String,Void,Void>{
+        String surl = "http://192.168.0.24/first/admin/retemp.do?humid=&busidx=5&temp=";
         URL url;
         HttpURLConnection urlConn;
         String speed;
-
-        public SendHttp(){}
-        public SendHttp(String speed){
+        public SendTempHumiHttp(){}
+        public SendTempHumiHttp(String speed){
             this.speed = speed;
             surl +=speed;
             try {
@@ -387,7 +661,7 @@ public class HomeFragment extends Fragment {
             }
         }
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Void doInBackground(String... params) {
             try {
                 urlConn = (HttpURLConnection) url.openConnection();
                 urlConn.getResponseCode();
@@ -398,4 +672,3 @@ public class HomeFragment extends Fragment {
         }
     }*/
 
-}
